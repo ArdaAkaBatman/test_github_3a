f014f8219092b7e8f051fe205d23a73ba82e551f
ArdaAkaBatman <arda.ucan@hak-reutte.ac.at>
2024-01-15 14:47:44 +0100

    second commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Uncle GitHub");
-		System.out.println("second test");
+		System.out.println("second Test");
 	}
 }
